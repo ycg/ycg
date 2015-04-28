072a5db26ad07ff7de129bae3072353443285a58
Tim <Tim@Tim-PC>
2015-04-28 22:54:39 +0800

    添加单元测试
    添加http请求下载文件的方法

---
+++ Ycg.Test/HttpHelperTest.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ycg.Util;
+
+namespace Ycg.Test
+{
+    [TestClass()]
+    public class HttpHelperTest
+    {
+        [TestMethod()]
+        public void DownloadFileTest()
+        {
+            string url = "http://img.plures.net/f1c8/9ce3/bcb8/e905/1cc0/e736/b022/161c.jpg";
+            string saveFilePath = @"C:\Users\Tim\Desktop\my.jpg";
+            HttpHelper.DownloadFile(url, saveFilePath);
+
+            if (File.Exists(saveFilePath))
+            {
+                Assert.IsTrue(true);
+            }
+        }
+    }
+}
---
+++ Ycg/Util/HttpHelper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using Ycg.Extension;
+
+namespace Ycg.Util
+{
+    public static class HttpHelper
+    {
+        public static void DownloadFile(string url, string saveFilePath)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            using (Stream stream = response.GetResponseStream())
+            {
+                if (stream == null)
+                    throw new NullReferenceException("The response stream is null.");
+                using (FileStream fileStream = new FileStream(saveFilePath, FileMode.Create))
+                {
+                    byte[] bytes = new byte[1024];
+                    int length = stream.Read(bytes, 0, bytes.Length);
+                    while (length > 0)
+                    {
+                        fileStream.Write(bytes, 0, length);
+                        length = stream.Read(bytes, 0, bytes.Length);
+                    }
+                }
+            }
+        }
+
+        public static string Post(string url, string data)
+        {
+            #region Create http request
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            request.Timeout = 1000 * 3;
+            request.ContentType = "application/x-www-form-urlencoded;charset=utf-8";
+            #endregion
+
+            if (request == null)
+                throw new NullReferenceException("The http request is null");
+            using (StreamWriter streamWriter = new StreamWriter(request.GetRequestStream(), Encoding.UTF8))
+            {
+                streamWriter.Write(data);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream responseStream = response.GetResponseStream();
+                if (responseStream == null)
+                    throw new NullReferenceException("The http response stream is null");
+                using (StreamReader streamReader = new StreamReader(responseStream))
+                {
+                    return streamReader.ReadToEnd();
+                }
+            }
+        }
+    }
+}
